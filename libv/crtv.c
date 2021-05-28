1d9cb37b6e84b7355b3581e0283a7f09e9362f1f
Sy Brand <tartanllama@gmail.com>
2021-05-28 19:26:37 +0100

    Comment crtv.c

--- libv/crtv.c
+++ libv/crtv.c
@@ -1,4 +1,7 @@
+//If the user writes a function called main it'll get mangled to vizh_main
 void vizh_main(char*);
+
+//We then provide a C entry point which allocates some tape for the main function to work on
 int main() {
     char primary_tape[4096];
     vizh_main(primary_tape);
