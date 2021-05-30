543908a47fe6e3077b916549fb68ed14215cd2fc
Sy Brand <tartanllama@gmail.com>
2021-05-30 18:57:48 +0100

    Silence linker output

--- vizh/linker.py
+++ vizh/linker.py
@@ -1,6 +1,8 @@
 import distutils.ccompiler
 import os.path
 import os
+import vizh.util
+import sys
 
 LIBV_NAME = 'libv.lib' if os.name == 'nt' else 'libv.a'
 CRTV_NAME = 'crtv.obj' if os.name == 'nt' else 'crtv.o'
@@ -23,4 +25,5 @@ class Linker(object):
         if link_crtv:
             object_files.append(os.path.join(vizh_path, CRTV_NAME))
 
-        c_compiler.link(c_compiler.EXECUTABLE, object_files, output_name)
+        with vizh.util.stdchannel_redirected(sys.stdout, os.devnull):
+            return c_compiler.link(c_compiler.EXECUTABLE, object_files, output_name)
